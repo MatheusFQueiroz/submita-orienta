870fc64fcf8999618cf7ca44aaf3cfc0f0d59ab7
MatheusFQueiroz <125527991+MatheusFQueiroz@users.noreply.github.com>
2025-10-02 19:57:48 -0300

    Refactor imports from ES6 to CommonJS syntax

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
-import express from "express";
-import helmet from "helmet";
-import cors from "cors";
-import { appRoutes } from "./presentation/routes";
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const { appRoutes } = require("./presentation/routes");
 
 const app = express();
 
@@ -10,7 +10,6 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-// Rotas principais
 app.use("/api", appRoutes);
 
-export { app };
+module.exports = app;
